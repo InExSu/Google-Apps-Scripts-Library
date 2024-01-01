3355dc49f293adec6b853e8e47f652ad0f060443
InExSu <InExSu@bk.ru>
2024-01-01 14:32:48 +0300

    2024-01-01

--- Lib_GAS_InExSu_DRAKON.ts
+++ Lib_GAS_InExSu_DRAKON.ts
@@ -1,8 +1,9 @@
-function z(){
-if (typeof DEBUG !== 'undefined') {
-    // DEBUG определена, ваш код здесь
-    console.log('DEBUG is defined:', DEBUG);
-} else {
-    // DEBUG не определена
-    console.log('DEBUG is not defined');
-}}
+function z() {
+    if (typeof DEBUG !== 'undefined') {
+        // DEBUG определена, ваш код здесь
+        console.log('DEBUG is defined:', DEBUG);
+    } else {
+        // DEBUG не определена
+        console.log('DEBUG is not defined');
+    }
+}
