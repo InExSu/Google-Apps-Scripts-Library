dd21c293f6f82712b780be6e2e1f951b4694a981
InExSu <InExSu@bk.ru>
2024-01-01 15:01:57 +0300

    2024-01-01

--- Lib_GAS_InExSu_DRAKON.ts
+++ Lib_GAS_InExSu_DRAKON.ts
@@ -12,6 +12,7 @@ function console_If(msg: string | object, debug = true) {
 }
 
 function range_Replace_In_Cells(range, regex, replacement) {
+    
     // item 45
       const sheet = range.getSheet()
       const values = range.getValues();
